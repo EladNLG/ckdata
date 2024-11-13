cfdef00593ca31b6461fa22d5cd9f04805605dfe
EladNLG <e1lad8955@gmail.com>
2024-11-13 20:00:37 +0200

    Update data generation

--- index.js
+++ index.js
@@ -6,6 +6,39 @@ function GenerateImage()
 {
     let file = document.getElementById("custom-file-upload").value;
 
+    let fileLines = file.split("\n")
+    console.log(fileLines.length)
+
+    let avgFrameRate = 0.0
+    let frequencies = Array(81)
+    let maxSpeedGains = Array(10)
+    let actualLen = 0
+    frequencies.fill(0,0,81)
+    maxSpeedGains.fill(0,0,10)
+    for (let i = 0; i < fileLines.length; i++)
+    {
+        let line = fileLines[i].split(",")
+        let lastCrouchTime = parseInt(line[0])
+        let jumpTime = parseInt(line[1])
+        let jumpSendTime = parseInt(line[2])
+        let speedGained = parseFloat(line[3]) * 0.09144 // convert from u/s to kmh
+        let framesPassed = parseInt(line[4])
+        let frameRate = parseFloat(line[5])
+        if (jumpSendTime > 60999)
+        {
+            continue
+        }
+        
+        actualLen++
+        console.log(parseFloat(frameRate))
+        frequencies[Math.floor(jumpSendTime / 1000) + 20]++
+        if (maxSpeedGains[framesPassed - 1] < speedGained)
+            maxSpeedGains[framesPassed - 1] = speedGained
+        avgFrameRate += frameRate
+    }
+    let highestFrequency = Math.max(...frequencies);
+    avgFrameRate /= actualLen
+
     let canvas = document.getElementById("canvas")
     let imgElem = document.getElementById("generated-image")
     let name = document.getElementById("name").value
@@ -24,7 +57,13 @@ function GenerateImage()
     ctx.textAlign = "center"
     ctx.textBaseline = "top"
     
-    ctx.font = "bold 32px JetBrains Mono"
+    let fontSize = 48
+    ctx.font = "bold " + fontSize + "px JetBrains Mono"
+    while (ctx.measureText(name).width > 180)
+    {
+        fontSize--
+        ctx.font = "bold " + fontSize + "px JetBrains Mono"
+    }
     ctx.fillText(name, 100, 20, 180)
 
     let rightSideMiddle = 500
@@ -36,28 +75,28 @@ function GenerateImage()
     // sidebar data
 
     ctx.fillStyle = "#aaa"
-    ctx.font = "300 24px JetBrains Mono"
+    ctx.font = "300 20px JetBrains Mono"
     ctx.fillText("FPS", leftSideMiddle, 120)
     
     ctx.fillStyle = "#f55"
     ctx.font = "800 24px JetBrains Mono"
-    ctx.fillText("144", leftSideMiddle, 150)
+    ctx.fillText(Math.round(avgFrameRate), leftSideMiddle, 150)
 
     ctx.fillStyle = "#aaa"
-    ctx.font = "300 24px JetBrains Mono"
+    ctx.font = "300 20px JetBrains Mono"
     ctx.fillText("Crouchkicks", leftSideMiddle, 220)
     
     ctx.fillStyle = "#f55"
     ctx.font = "800 24px JetBrains Mono"
-    ctx.fillText("3194", leftSideMiddle, 250)
+    ctx.fillText(actualLen, leftSideMiddle, 250)
 
     ctx.fillStyle = "#aaa"
-    ctx.font = "300 24px JetBrains Mono"
-    ctx.fillText("Made by", leftSideMiddle, 320)
+    ctx.font = "300 20px JetBrains Mono"
+    ctx.fillText("Avg. Buf. Time", leftSideMiddle, 320)
     
     ctx.fillStyle = "#f55"
     ctx.font = "800 24px JetBrains Mono"
-    ctx.fillText("EladNLG", leftSideMiddle, 350)
+    ctx.fillText("1.65ms", leftSideMiddle, 350)
 
     // FREQUENCY
 
@@ -72,7 +111,8 @@ function GenerateImage()
     
     for (let i = 0; i < 81; i++)
     {
-        let l = Math.random()
+        let l = frequencies[i] / highestFrequency
+        l = Math.min(1, Math.max(l, 0))
         ctx.fillRect(rightSideMiddle - 244 + 2 + 6 * i, graphYOffset - 120 * l, 4, 120 * l)
     }
     
@@ -81,7 +121,7 @@ function GenerateImage()
     for (let i = 0; i < 9; i++)
     {
         let l = Math.random()
-        ctx.fillText(i * 10, rightSideMiddle - 240 + 60 * i, graphYOffset + 10 )
+        ctx.fillText(i * 10 - 20, rightSideMiddle - 240 + 60 * i, graphYOffset + 10 )
     }
 
     // SPEED GAIN
@@ -89,36 +129,26 @@ function GenerateImage()
     graphYOffset = 100
 
     ctx.font = "18px JetBrains Mono"
-    ctx.fillText("Speed Gain", rightSideMiddle, graphYOffset - 25)
+    ctx.fillText("Max Speed Gain", rightSideMiddle, graphYOffset - 25)
     
     ctx.fillRect(rightSideMiddle - 244, graphYOffset + 120, 488, 4); 
 
     ctx.fillRect(rightSideMiddle - 244, graphYOffset, 4, 120);
 
+    console.log(maxSpeedGains.length)
     ctx.beginPath()
-    ctx.moveTo(rightSideMiddle - 242, graphYOffset + 10)
-    for (let i = 1; i < 6; i++)
+    ctx.moveTo(rightSideMiddle - 242, graphYOffset + 120 - 110 * (maxSpeedGains[0] / 13.0))
+    for (let i = 1; i < maxSpeedGains.length + 1; i++)
     {
-        ctx.lineTo(rightSideMiddle - 244 + 485 * (i / 5), graphYOffset + 120 - 110 * (Math.pow((5 - (i - 1)) / 5, 2)))
-        ctx.lineTo(rightSideMiddle - 244 + 485 * (i / 5), graphYOffset + 120 - 110 * (Math.pow((5 - i) / 5, 2)))
+        console.log(graphYOffset + 120 - 110 * (maxSpeedGains[i - 1] / 13.0))
+        ctx.lineTo(rightSideMiddle - 244 + 485 * (i / maxSpeedGains.length), graphYOffset + 120 - 110 * (maxSpeedGains[i - 1] / 13.0))
+        ctx.lineTo(rightSideMiddle - 244 + 485 * (i / maxSpeedGains.length), graphYOffset + 120 - 110 * (i >= maxSpeedGains.length ? 0 : maxSpeedGains[i] / 13.0))
     }
     ctx.lineWidth = 2
+    ctx.fillStyle = "#fff"
     ctx.strokeStyle = "#fff"
     ctx.stroke()
     
-    ctx.fillStyle = "#000"
-    for (let i = 0; i < 5; i++)
-    {
-        let x = rightSideMiddle - 244 + 485 * (i + 0.5) / 5
-        let y = graphYOffset + 120 - 110 * (Math.pow((5 - i) / 5, 2))
-        let height = Math.max(110 * Math.random() * Math.pow((5 - i) / 5, 2), 2)
-        let thickness = 2
-        let width = 32
-        ctx.fillRect(x - thickness / 2, y - height / 2, thickness, height)
-        ctx.fillRect(x - width / 2, y - height / 2 - thickness, width, thickness)  
-        ctx.fillRect(x - width / 2, y + height / 2, width, thickness)  
-    }
-    ctx.fillStyle = "#fff"
 
     ctx.font = "12px JetBrains Mono"
     ctx.textAlign = "right"
